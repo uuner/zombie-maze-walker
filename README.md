96a0d8d475415833b214c806be5e9f06acc993f2
uuner <julia.jomantaite@gmail.com>
2018-12-29 15:50:30 -0500

    initial

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# zombie-maze-walker
-A short shell script about zombies and mazes 
+# Zombie-maze-walker
+
+The objective of the game is to exit the maze avoiding the zombies.
+
+To play the game with the default maze open a terminal window 
+and run the script:
+
+```
+./zombie-maze-walker.sh
+```
+
+To play with some other maze pass the name of maze file
+in its first argument. For example:
+```
+./zombie-maze-walker.sh smallmaze.txt
+```
+
+## Requirements
+
+The game should run on any system with bash and GNU sed
+and standard Unix tools.
+
+## Screenshot
+
+```
++--+--+--+--+--+
+|  |     |     |
++  +  +  +--+  +
+|  |  |   Z  > |
++  +Z +--+  +  +
+|  |  |  |  |  |
++  +  +  +  +  +
+|     |     |  |
++--+--+--+--+  +
+|               
++--+--+--+--+--+
+```
+
+## Building a custom maze
+
+You can build your own maze. The game assumes the maze is of rectangular shape,
+so keep the length of all lines equal. The character is represented by one of 
+``><^v`` characters. Zombies are one of the ``udlr`` letters. The exit must
+be on the outer border.
---
+++ zombie-maze-walker.sh
@@ -0,0 +1,115 @@
+#!/usr/bin/env bash
+
+IFS=''
+
+MAZEFILE="maze.txt"
+if [ -f "$1" ]; then
+  MAZEFILE="$1"
+fi
+
+MAZE=$(sed 's#[/\\]#\\&#g;s#^#s/$/#;s#$#~/#;$s#~/\$#/#' "$MAZEFILE")
+
+GAMEFILE=$(tempfile) || exit
+trap '{ rm -f $GAMEFILE; }' EXIT
+
+WIDTH=$(read line < $MAZEFILE; echo -n "$line" | wc -c)
+
+cat << EOF > $GAMEFILE
+
+#!/bin/sed -nf
+
+1{
+s/.*//
+$MAZE
+h; b display
+}
+
+/^\(\[A\)\|w/{x; s/[><v]/^/; x; b next;}
+/^\(\[B\)\|s/{x; s/[><^]/v/; x; b next;}
+/^\(\[D\)\|a/{x; s/[>^v]/</; x; b next;}
+/^\(\[C\)\|d/{x; s/[<v^]/>/; x; b next;}
+
+:next
+
+x
+# Losing condition #1 - you walk into a zombie
+/\([udlr]\(.\{$WIDTH\}\)\^\)\|\(v\(.\{$WIDTH\}\)[udlr]\)\|\(>[udlr]\|[udlr]<\)/{
+s/$/&~ZOMBIE SAYS YUM!!!/
+x
+b display
+}
+# character movements
+s/ \(.\{$WIDTH\}\)\^/^\1 /
+s/v\(.\{$WIDTH\}\) / \1v/
+s/> / >/
+s/ </< /
+
+# Losing condition #2 - a zombie walks into you
+/\([><v^].\{$WIDTH\}u\)\|\(d.\{$WIDTH\}[><v^]\)\|\(r[><v^]\)\|\([><v^]l\)/{
+s/$/&~YUM BRAINZZZ!!!/
+x
+b display
+}
+
+# nemesis movements
+# turning around
+s/\([^ ].\{$WIDTH\}\)u/\1d/g
+s/d\(.\{$WIDTH\}[^ ]\)/u\1/g
+s/r\([^ ]\)/l\1/g
+s/\([^ ]\)l/\1r/g
+
+# moving
+s/ \(.\{$WIDTH\}\)u/u\1 /g
+s/d\(.\{$WIDTH\}\) / \1d/g
+s/r / r/g
+s/ l/l /g
+
+# winning condition
+/\(^[^~]*\^\)\|\(v[^~]*$\)\|\(>~\)\|\(~<\)/s/$/~YOU WON!!! HAVE A COOKIE/
+
+x
+:display
+g
+s/~/\\
+/g
+
+s/[><^v]/[1;36m&[m/g
+s/[udlr]/[1;32mZ[m/g
+
+i\\
+[2J[H
+p
+/!/q
+
+EOF
+#-------------
+# Play phase 
+
+mytime="`date +%s`"
+
+export LC_ALL="C"
+(while true;
+do
+  read -s -t 1 -n 1 TMP
+  RES=$?
+  CMD="$TMP"
+  if [ "$CMD" == '' ]
+  then
+    read -s -n 1 TMP; CMD="$CMD$TMP";
+    if [ "$CMD" == '[' ]
+    then
+      read -s -n 1 TMP; CMD="$CMD$TMP";
+    fi
+  fi
+  if [ $RES -eq 0 ]
+  then
+    echo "$CMD"
+    continue
+  fi
+  mytimenew="`date +%s`"
+  if [ "$mytimenew" != "$mytime" ]
+  then
+    echo
+    mytime=$mytimenew
+  fi
+done) | sed -nf $GAMEFILE
